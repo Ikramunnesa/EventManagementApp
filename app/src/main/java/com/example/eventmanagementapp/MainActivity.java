c17c56e04d56960e445c2304d2cec3969402c083
Ikramunnesa <ikraafi@gmail.com>
2019-03-27 21:51:03 +0600

    modified

--- app/src/main/java/com/example/eventmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/eventmanagementapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.eventmanagementapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        EventListFragment eventListFragment = new EventListFragment();
+        ft.add(R.id.fragmentContainer, eventListFragment);
+        ft.commit();
     }
 }
