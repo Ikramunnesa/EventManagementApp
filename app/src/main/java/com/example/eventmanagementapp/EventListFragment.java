e372a62d5a288edf29d174025373e55f21d74f06
Ikramunnesa <ikraafi@gmail.com>
2019-03-28 17:28:00 +0600

    file modified

--- app/src/main/java/com/example/eventmanagementapp/EventListFragment.java
+++ app/src/main/java/com/example/eventmanagementapp/EventListFragment.java
@@ -1,9 +1,11 @@
 package com.example.eventmanagementapp;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +13,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.eventmanagementapp.eventdb.EventDateSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +27,19 @@ public class EventListFragment extends Fragment {
     private RecyclerView recyclerView;
     private EventAdapter adapter;
     private List<Event>eventList = new ArrayList<>();
-
+    private FloatingActionButton fab;
+    private AddNewEventListener listener;
+    private EventDateSource dateSource;
 
     public EventListFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (AddNewEventListener) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,11 +52,26 @@ public class EventListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = view.findViewById(R.id.recyclerview);
+        fab = view.findViewById(R.id.fab);
+        dateSource = new EventDateSource(getActivity());
+        eventList = dateSource.getAllEventsAsc();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         GridLayoutManager glm = new GridLayoutManager(getActivity(), 2);
         recyclerView.setLayoutManager(glm);
         adapter = new EventAdapter(getActivity(),eventList);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onAddNewEvent();
+            }
+        });
+    }
+
+    interface AddNewEventListener{
+       void onAddNewEvent();
+       void onComplete();
     }
 }
